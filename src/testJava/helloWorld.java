bb26d7180c20a8c82ebce8e3b1f7b83db5c8cbbd
IT24102358 <IT24102358@my.sliit.lk>
2025-05-16 16:57:52 +0530

    feat: Created helloWorld.java

--- src/testJava/helloWorld.java
+++ src/testJava/helloWorld.java
@@ -2,6 +2,6 @@ package testJava;
 
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World bn!");
     }
 }
